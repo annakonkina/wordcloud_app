dca98e1fbebc9137a0acde9d45acdb39d760767f
Anya Konkina <annakonkina3998@yandex.ru>
2023-03-07 18:03:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,141 +68,136 @@ if uploaded_file and sheet_name:
                    sheet_name=sheet_name,
                 #    usecols='A:F',
                    header=0)
-else:
-    df = pd.DataFrame().from_dict({'uid':[0],
-                                   'answer':['Bla bla bla'],
-                                    'question':['???']        
-                                                        })
 
-# aggrid
-gd = GridOptionsBuilder.from_dataframe(df)
-gd.configure_pagination(enabled=True)
-gd.configure_default_column(groupable=True)
-gd.configure_selection(selection_mode='single',
-                    #    use_ckeckbox=True
-                        )
-gridOptions = gd.build()
-grid_table = AgGrid(df,
-                    gridOptions = gridOptions,
-        fit_columns_on_grid_load=True,
-        height=500,
-        width='100%',
-        theme='streamlit',
-        update_mode = GridUpdateMode.GRID_CHANGED,
-        reload_data=True,
-        allow_unsafe_jscode=True,
-        editable=True)
-df_interactive = grid_table['data']
+    # aggrid
+    gd = GridOptionsBuilder.from_dataframe(df)
+    gd.configure_pagination(enabled=True)
+    gd.configure_default_column(groupable=True)
+    gd.configure_selection(selection_mode='single',
+                        #    use_ckeckbox=True
+                            )
+    gridOptions = gd.build()
+    grid_table = AgGrid(df,
+                        gridOptions = gridOptions,
+            fit_columns_on_grid_load=True,
+            height=500,
+            width='100%',
+            theme='streamlit',
+            update_mode = GridUpdateMode.GRID_CHANGED,
+            reload_data=True,
+            allow_unsafe_jscode=True,
+            editable=True)
+    df_interactive = grid_table['data']
 
 
-    # image = Image.open('images/hands-keyboard.jpg')
-    # st.image(image,
-    #         #  caption='got from Freepick',
-    #         #  use_column_width=True,
-    #         width = 200
-    #         )
-    # col2.dataframe(df)
+        # image = Image.open('images/hands-keyboard.jpg')
+        # st.image(image,
+        #         #  caption='got from Freepick',
+        #         #  use_column_width=True,
+        #         width = 200
+        #         )
+        # col2.dataframe(df)
 
-    # SELECTION BOX AND WORDCLOUD
-col1, col2 = st.columns(2)
+        # SELECTION BOX AND WORDCLOUD
+    col1, col2 = st.columns(2)
 
-nb_cols = len([i for i in df.columns if i not in ['uid', 'answer']])
-df_cols = [i for i in df.columns if i not in ['uid', 'answer']]
-mask = []
-for i in range(nb_cols):
-        globals()[f'{i}_options'] = df[df_cols[i]].unique().tolist()
-        globals()[f'{i}_selection'] = col1.multiselect(f'{df_cols[i]}:',
-                                globals()[f'{i}_options'],
-                                default = globals()[f'{i}_options'])
+    nb_cols = len([i for i in df.columns if i not in ['uid', 'answer']])
+    df_cols = [i for i in df.columns if i not in ['uid', 'answer']]
+    mask = []
+    for i in range(nb_cols):
+            globals()[f'{i}_options'] = df[df_cols[i]].unique().tolist()
+            globals()[f'{i}_selection'] = col1.multiselect(f'{df_cols[i]}:',
+                                    globals()[f'{i}_options'],
+                                    default = globals()[f'{i}_options'])
 
-language = col2.text_input('Stopwords of which language do you want to use? \
-                            (type f.e. "english", "french" etc)', key = 'language')
-stopwords_to_add = col2.text_input('What stopwords do you want to add? \
-                                   (type words separated by commas or press enter)', 
-                                   key = 'stopwords_add')
-stopwords_to_remove = col2.text_input('What stopwords you would like to remove? \
-                                      (type words separated by commas or press enter)', 
-                                      key = 'stopwords_remove')
+    language = col2.text_input('Stopwords of which language do you want to use? \
+                                (type f.e. "english", "french" etc)', key = 'language')
+    stopwords_to_add = col2.text_input('What stopwords do you want to add? \
+                                    (type words separated by commas or press enter)', 
+                                    key = 'stopwords_add')
+    stopwords_to_remove = col2.text_input('What stopwords you would like to remove? \
+                                        (type words separated by commas or press enter)', 
+                                        key = 'stopwords_remove')
 
-# --- FILTER DATAFRAME BASED ON SELECTION
-for i in range(nb_cols):
-        mask.append((df[df_cols[i]].isin(globals()[f'{i}_selection'])))
+    # --- FILTER DATAFRAME BASED ON SELECTION
+    for i in range(nb_cols):
+            mask.append((df[df_cols[i]].isin(globals()[f'{i}_selection'])))
 
-df_filtered = df.copy()
+    df_filtered = df.copy()
 
-if 'df_filtered' not in st.session_state:
-    st.session_state.df_filtered = df_filtered
+    if 'df_filtered' not in st.session_state:
+        st.session_state.df_filtered = df_filtered
 
-for cond in mask:
-        df_filtered = df_filtered[cond]
+    for cond in mask:
+            df_filtered = df_filtered[cond]
 
-df_filtered['answer'] = df_filtered['answer'].fillna('-')
+    df_filtered['answer'] = df_filtered['answer'].fillna('-')
 
-number_of_result = df_filtered.shape[0]
-col2.markdown(f'**Available results:** {number_of_result}')
+    number_of_result = df_filtered.shape[0]
+    col2.markdown(f'**Available results:** {number_of_result}')
 
-# STOPWORDS
-stop_words = set(stopwords.words('english'))
+    # STOPWORDS
+    stop_words = set(stopwords.words('english'))
 
-if language != 'english' and language != '':
-        stop_words = set(stopwords.words(language))
+    if language != 'english' and language != '':
+            stop_words = set(stopwords.words(language))
 
-stopwords_to_add_set = set([i.strip().lower() for i in stopwords_to_add.split(',')])
-stopwords_to_remove_set = set([i.strip().lower() for i in stopwords_to_remove.split(',')])
+    stopwords_to_add_set = set([i.strip().lower() for i in stopwords_to_add.split(',')])
+    stopwords_to_remove_set = set([i.strip().lower() for i in stopwords_to_remove.split(',')])
 
-if len(stopwords_to_add_set) > 0 and stopwords_to_add_set != set(['']):
-    stop_words.update(stopwords_to_add_set)
-if len(stopwords_to_remove_set) > 0 and stopwords_to_remove_set != set(['']):
-    stop_words = stop_words - stopwords_to_remove_set
+    if len(stopwords_to_add_set) > 0 and stopwords_to_add_set != set(['']):
+        stop_words.update(stopwords_to_add_set)
+    if len(stopwords_to_remove_set) > 0 and stopwords_to_remove_set != set(['']):
+        stop_words = stop_words - stopwords_to_remove_set
 
-# ---- ADD WORDCLOUD
-col2.text(f'Number of empty answers in the data: {df_filtered.answer.isna().sum()}') 
-corpus = df_filtered.answer.unique().tolist()
-corpus = [i.lower() for i in corpus]
-text = ' '.join(corpus)
+    # ---- ADD WORDCLOUD
+    col2.text(f'Number of empty answers in the data: {df_filtered.answer.isna().sum()}') 
+    corpus = df_filtered.answer.unique().tolist()
+    corpus = [i.lower() for i in corpus]
+    text = ' '.join(corpus)
 
-for i in ['-', '  ', '’', "\'"]: # drop extra symbols
-        text = text.replace(i, '')
-# st.text(text)
-text = text.translate(str.maketrans('', '', string.punctuation))
+    for i in ['-', '  ', '’', "\'"]: # drop extra symbols
+            text = text.replace(i, '')
+    # st.text(text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
 
-# LEMMATIZE
-try:
-        text = lemmatize_sentence(text)
-except:
-        nltk.download('all')
-        text = lemmatize_sentence(text)
+    # LEMMATIZE
+    try:
+            text = lemmatize_sentence(text)
+    except:
+            nltk.download('all')
+            text = lemmatize_sentence(text)
 
-# Create and generate a word cloud image:
+    # Create and generate a word cloud image:
 
-wordcloud = WordCloud(background_color='white',
-                width=1600, height=1000, 
-                max_words=len(text),
-                max_font_size=210, 
-                relative_scaling=.01,
-                collocations=False,
-                stopwords = stop_words).generate(text)
+    wordcloud = WordCloud(background_color='white',
+                    width=1600, height=1000, 
+                    max_words=len(text),
+                    max_font_size=210, 
+                    relative_scaling=.01,
+                    collocations=False,
+                    stopwords = stop_words).generate(text)
 
-# Display the generated image:
-# https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html
-st.set_option('deprecation.showPyplotGlobalUse', False)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-# Save to file first or an image file has already existed.
-wc = 'wordcloud.png'
-plt.savefig(wc, pad_inches=None , dpi=1200)
-col2.pyplot()
+    # Display the generated image:
+    # https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    # Save to file first or an image file has already existed.
+    wc = 'wordcloud.png'
+    plt.savefig(wc, pad_inches=None , dpi=1200)
+    col2.pyplot()
 
 
-with open(wc, "rb") as img:
-        btn = col2.download_button(
-                label="Download image",
-                data=img,
-                file_name=wc,
-                mime="image/png",
-                
-        )
+    with open(wc, "rb") as img:
+            btn = col2.download_button(
+                    label="Download image",
+                    data=img,
+                    file_name=wc,
+                    mime="image/png",
+                    
+            )
 
 # #  --- GROUP DATAFRAME
 # df_grouped = df[mask].groupby('experiment_name').uid.nunique().reset_index()
