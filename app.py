cc779b7206c111415db8a9d2f37533120d57fcf5
Anya Konkina <annakonkina3998@yandex.ru>
2023-03-07 17:20:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,138 +62,138 @@ st.markdown('The excel table should have columns: uid, answer, question, experim
 uploaded_file = st.file_uploader('Drag a verbatims file here', type=['xlsx'], key="uploaded_file")
 sheet_name = st.text_input('Type which sheet you want to open', key = 'sheet_name')
 
-# Initialization
-if 'sheet_name' not in st.session_state:
-    st.session_state['sheet_name'] = 0
-if 'uploaded_file' not in st.session_state:
-    st.session_state['uploaded_file'] = './verbatims.xlsx'
 
-
-df = pd.read_excel(st.session_state.uploaded_file,
+if uploaded_file and sheet_name:
+        df = pd.read_excel(uploaded_file,
                    sheet_name=sheet_name,
                 #    usecols='A:F',
                    header=0)
+        st.session_state['df'] = df
 
-if uploaded_file and sheet_name:
-        
+# Initialization
+if 'df' not in st.session_state:
+    st.session_state['df'] = pd.DataFrame().from_dict({'uid':[0],
+                                                       'answer':['Bla bla bla'],
+                                                        'question':['???']        
+                                                        })
 
-        # aggrid
-        gd = GridOptionsBuilder.from_dataframe(df)
-        gd.configure_pagination(enabled=True)
-        gd.configure_default_column(groupable=True)
-        gd.configure_selection(selection_mode='single',
-                            #    use_ckeckbox=True
-                               )
-        gridOptions = gd.build()
-        grid_table = AgGrid(df,
-                            gridOptions = gridOptions,
-                fit_columns_on_grid_load=True,
-                height=500,
-                width='100%',
-                theme='streamlit',
-                update_mode = GridUpdateMode.GRID_CHANGED,
-                reload_data=True,
-                allow_unsafe_jscode=True,
-                editable=True)
-        df_interactive = grid_table['data']
+    # aggrid
+    gd = GridOptionsBuilder.from_dataframe(st.session_state.df)
+    gd.configure_pagination(enabled=True)
+    gd.configure_default_column(groupable=True)
+    gd.configure_selection(selection_mode='single',
+                        #    use_ckeckbox=True
+                            )
+    gridOptions = gd.build()
+    grid_table = AgGrid(st.session_state.df,
+                        gridOptions = gridOptions,
+            fit_columns_on_grid_load=True,
+            height=500,
+            width='100%',
+            theme='streamlit',
+            update_mode = GridUpdateMode.GRID_CHANGED,
+            reload_data=True,
+            allow_unsafe_jscode=True,
+            editable=True)
+    df_interactive = grid_table['data']
 
 
-        # image = Image.open('images/hands-keyboard.jpg')
-        # st.image(image,
-        #         #  caption='got from Freepick',
-        #         #  use_column_width=True,
-        #         width = 200
-        #         )
-        # col2.dataframe(df)
+    # image = Image.open('images/hands-keyboard.jpg')
+    # st.image(image,
+    #         #  caption='got from Freepick',
+    #         #  use_column_width=True,
+    #         width = 200
+    #         )
+    # col2.dataframe(df)
 
-        # SELECTION BOX AND WORDCLOUD
-        col1, col2 = st.columns(2)
+    # SELECTION BOX AND WORDCLOUD
+    col1, col2 = st.columns(2)
 
-        nb_cols = len([i for i in df.columns if i not in ['uid', 'answer']])
-        df_cols = [i for i in df.columns if i not in ['uid', 'answer']]
-        mask = []
-        for i in range(nb_cols):
-                globals()[f'{i}_options'] = df[df_cols[i]].unique().tolist()
-                globals()[f'{i}_selection'] = col1.multiselect(f'{df_cols[i]}:',
-                                        globals()[f'{i}_options'],
-                                        default = globals()[f'{i}_options'])
-        
-        # --- FILTER DATAFRAME BASED ON SELECTION
-        for i in range(nb_cols):
-                mask.append((df[df_cols[i]].isin(globals()[f'{i}_selection'])))
+    nb_cols = len([i for i in df.columns if i not in ['uid', 'answer']])
+    df_cols = [i for i in df.columns if i not in ['uid', 'answer']]
+    mask = []
+    for i in range(nb_cols):
+            globals()[f'{i}_options'] = df[df_cols[i]].unique().tolist()
+            globals()[f'{i}_selection'] = col1.multiselect(f'{df_cols[i]}:',
+                                    globals()[f'{i}_options'],
+                                    default = globals()[f'{i}_options'])
+    
+    # --- FILTER DATAFRAME BASED ON SELECTION
+    for i in range(nb_cols):
+            mask.append((df[df_cols[i]].isin(globals()[f'{i}_selection'])))
 
-        df_filtered = df.copy()
-        for cond in mask:
-                df_filtered = df_filtered[cond]
-        
-        number_of_result = df_filtered.shape[0]
-        col2.markdown(f'**Available results:** {number_of_result}')
+    df_filtered = st.session_state.df.copy()
+    for cond in mask:
+            df_filtered = df_filtered[cond]
+    
+    number_of_result = df_filtered.shape[0]
+    col2.markdown(f'**Available results:** {number_of_result}')
 
-        # STOPWORDS
-        stop_words = set(stopwords.words('english'))
-        language = col2.text_input('Stopwords of which language do you want to use? \
-                                 (type f.e. "english", "french" etc)')
-        if language != 'english' and language != '':
-              stop_words = set(stopwords.words(language))
+    # STOPWORDS
+    stop_words = set(stopwords.words('english'))
+    language = col2.text_input('Stopwords of which language do you want to use? \
+                                (type f.e. "english", "french" etc)')
+    if language != 'english' and language != '':
+            stop_words = set(stopwords.words(language))
 
-        stopwords_to_add = col2.text_input('What stopwords do you want to add? (type words separated by commas)')
-        stopwords_to_remove = col2.text_input('What stopwords you would like to remove? (type words separated by commas)')
-        stopwords_to_add_set = set([i.strip().lower() for i in stopwords_to_add.split(',')])
-        stopwords_to_remove_set = set([i.strip().lower() for i in stopwords_to_remove.split(',')])
-        if len(stopwords_to_add_set) > 0 and stopwords_to_add_set != set(['']):
-            stop_words.update(stopwords_to_add_set)
-        if len(stopwords_to_remove_set) > 0 and stopwords_to_remove_set != set(['']):
-            stop_words = stop_words - stopwords_to_remove_set
-        
-        # ---- ADD WORDCLOUD
-        col2.text(f'Number of empty answers in the data: {df_filtered.answer.isna().sum()}') 
-        df_filtered['answer'] = df_filtered['answer'].fillna('-')
-        corpus = df_filtered.answer.unique().tolist()
-        corpus = [i.lower() for i in corpus]
-        text = ' '.join(corpus)
-        
-        for i in ['-', '  ', '’', "\'"]: # drop extra symbols
-                text = text.replace(i, '')
-        # st.text(text)
-        text = text.translate(str.maketrans('', '', string.punctuation))
-        
-        # LEMMATIZE
-        try:
-              text = lemmatize_sentence(text)
-        except:
-              nltk.download('all')
-              text = lemmatize_sentence(text)
+    stopwords_to_add = col2.text_input('What stopwords do you want to add? (type words separated by commas)')
+    stopwords_to_remove = col2.text_input('What stopwords you would like to remove? (type words separated by commas)')
+    stopwords_to_add_set = set([i.strip().lower() for i in stopwords_to_add.split(',')])
+    stopwords_to_remove_set = set([i.strip().lower() for i in stopwords_to_remove.split(',')])
+    if len(stopwords_to_add_set) > 0 and stopwords_to_add_set != set(['']):
+        stop_words.update(stopwords_to_add_set)
+    if len(stopwords_to_remove_set) > 0 and stopwords_to_remove_set != set(['']):
+        stop_words = stop_words - stopwords_to_remove_set
+    
+    # ---- ADD WORDCLOUD
+    col2.text(f'Number of empty answers in the data: {df_filtered.answer.isna().sum()}') 
+    df_filtered['answer'] = df_filtered['answer'].fillna('-')
+    corpus = df_filtered.answer.unique().tolist()
+    corpus = [i.lower() for i in corpus]
+    text = ' '.join(corpus)
+    
+    for i in ['-', '  ', '’', "\'"]: # drop extra symbols
+            text = text.replace(i, '')
+    # st.text(text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    
+    # LEMMATIZE
+    try:
+            text = lemmatize_sentence(text)
+    except:
+            nltk.download('all')
+            text = lemmatize_sentence(text)
 
-        # Create and generate a word cloud image:
+    # Create and generate a word cloud image:
 
-        wordcloud = WordCloud(background_color='white',
-                        width=1600, height=1000, 
-                        max_words=len(text),
-                        max_font_size=210, 
-                        relative_scaling=.01,
-                        collocations=False,
-                        stopwords = stop_words).generate(text)
+    wordcloud = WordCloud(background_color='white',
+                    width=1600, height=1000, 
+                    max_words=len(text),
+                    max_font_size=210, 
+                    relative_scaling=.01,
+                    collocations=False,
+                    stopwords = stop_words).generate(text)
 
-        # Display the generated image:
-        # https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html
-        st.set_option('deprecation.showPyplotGlobalUse', False)
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis("off")
-        plt.show()
-        # Save to file first or an image file has already existed.
-        wc = 'wordcloud.png'
-        plt.savefig(wc, pad_inches=None , dpi=1200)
-        col2.pyplot()
+    # Display the generated image:
+    # https://matplotlib.org/stable/tutorials/intermediate/imshow_extent.html
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    # Save to file first or an image file has already existed.
+    wc = 'wordcloud.png'
+    plt.savefig(wc, pad_inches=None , dpi=1200)
+    col2.pyplot()
 
-        
-        with open(wc, "rb") as img:
-                btn = col2.download_button(
-                        label="Download image",
-                        data=img,
-                        file_name=wc,
-                        mime="image/png",
-                        
-                )
+    
+    with open(wc, "rb") as img:
+            btn = col2.download_button(
+                    label="Download image",
+                    data=img,
+                    file_name=wc,
+                    mime="image/png",
+                    
+            )
 
 # #  --- GROUP DATAFRAME
 # df_grouped = df[mask].groupby('experiment_name').uid.nunique().reset_index()
