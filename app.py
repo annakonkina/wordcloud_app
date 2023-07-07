7abc96380083c9b2f0d3c82d97f4a8753d7f5ad3
Anya Konkina <annakonkina3998@yandex.ru>
2023-07-07 14:38:14 +0300

    Update app.py
    Adding a piece of code that makes it possible to fix the issue with formats. So from now on if the file has Summary sheet and the link cell has a name 'Go to summary' it will transform the data to the appropriate format for the app.

--- app.py
+++ app.py
@@ -113,6 +113,14 @@ if 'uploaded_file' in st.session_state and 'sheet_name' in st.session_state:
                             sheet_name=st.session_state.sheet_name,
                         #    usecols='A:F',
                             header=0)
+            
+            # in case the uploaded file has a structure of excel with LINKS and the button = 'Go to Summary':
+            df = df.dropna(how = 'all').reset_index(drop=True)
+            if df.iloc[0, 0] == 'Go to Summary':
+                df.columns = df.iloc[0, :].values
+                df = df.iloc[1:, :]
+                df = df.drop(columns = ['Go to Summary'])
+
             st.session_state.df  = df
         else:
             if 'df' not in st.session_state:
